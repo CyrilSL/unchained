238449f456cb6c5431172460619239d18e8b8b3c
cyrilsl <lucascyrilsamuel@gmail.com>
2019-09-23 00:08:40 +0530

    crushin

--- forum/files/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import File_Object
-
-admin.site.register(File_Object)
--- forum/files/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import File_Object
-from django.contrib.auth.models import User
-
-class NewFileForm(forms.ModelForm):
-    class Meta:
-        model = File_Object
-        fields = ['name', 'data']
